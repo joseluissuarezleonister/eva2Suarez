8cd530fdf9bb8a289bfb855cf7893fcf34f4c554
JoseSuarez <joseluissuarezleon@gmail.com>
2026-03-07 08:54:19 -0500

    fix: añadida dependencia gson

--- app/src/main/java/jose/suarez/com/josesuarezeva2/Api/RetrofitClient.kt
+++ app/src/main/java/jose/suarez/com/josesuarezeva2/Api/RetrofitClient.kt
@@ -1,5 +1,8 @@
 package jose.suarez.com.josesuarezeva2.Api
 
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
 object RetrofitClient {
     private const val BASE_URL = "https://69ab0c98e051e9456fa3308b.mockapi.io/api/eva2/"
 
